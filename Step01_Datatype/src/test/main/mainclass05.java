d7e8986dbe22eff73dcb64b3f5dbf62c180c6077
yoonjaewon <jikim1032@naver.com>
2025-06-16 16:54:51 +0900

    mainclass05 added

---
+++ Step01_Datatype/src/test/main/mainclass05.java
@@ -0,0 +1,21 @@
+package test.main;
+
+/*
+ * [Java 기본 데이터 type]
+ *  string type
+ *  -문자열을 다룰때 사용하는 데이타 type 이다.
+ * 
+ * 
+ * 
+ * 
+ */
+public class mainclass05 {
+	public static void main(String[] args) {
+		String name = "김구라";
+		String tmp = name;   
+		name = "원숭이";
+		name = null;
+
+	}
+
+}
