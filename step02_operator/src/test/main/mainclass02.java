b2d00faf9948b79dfdf6ff936f9b8bc1098d5d57
yoonjaewon <jikim1032@naver.com>
2025-06-17 16:57:06 +0900

    step2_operator

---
+++ step02_operator/src/test/main/mainclass02.java
@@ -0,0 +1,27 @@
+package test.main;
+
+public class mainclass02 {
+	public static void main(String[] args) {
+		int num=0;
+		num++;
+		num++;
+		num++; //num이 최종적으로 3이 된다.
+		
+		int num2=0;
+		num2--;
+		num2--;
+		num2--; // num2 가 최종적으로 -3이 된다
+		
+		int num3 = 0;
+		int result1 = num3++; // result 에 0이 대입되고 num3 가 1 이 증가한다.
+		
+		
+		int num4=0;
+		int result2 = ++num4; // num4가 1 증가하고 result2에 1 이 증가한다.
+		
+		
+		
+		
+	}
+
+}
---
+++ step02_operator/src/test/main/mainclass03.java
@@ -0,0 +1,33 @@
+package test.main;
+
+public class mainclass03 {
+	public static void main(String[] args) {
+		/*
+		 * i라는 변수를 만들고 초기값 0 대입
+		 * i가 10보다는 작은 동안에 반복한다(작으면 for문 블럭을 실행한다.)
+		 * i를 1씩 증가 시키면서 
+		 * i는 for문 안에서 만들어진 지역변수(for문이 종료되면 i는 사라진다.)
+		 * 
+		 */
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+			// int i=0 최초 한번 실행
+			//i++증가,10보다 크지 않을 때 까지 순서대로 증가
+		}
+		
+		
+		//콘솔창에 9 부터 0까지 순서대로 출력되도록 for문을 구성해 보세요
+		
+		/*
+		 * i라는 변수를 만들고 초기값 9 대입
+		 * i가 0이상인 동안에 반복한다.
+		 * i를 1씩 감소시키면서
+		 * 
+		 */
+		for (int i=9; i>=0 ; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
